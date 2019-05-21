2f213c3cc6cd9b48c71e0fe0052ee874bca42d4f
logan169 <logan1691987@gmail.com>
2019-05-21 09:46:14 -0400

    Added shebang

--- python/find_animated_sequences_frame_range.py
+++ python/find_animated_sequences_frame_range.py
@@ -1,10 +1,5 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
-
-# General Python test:
-#     write a function that finds all animated sequences in a given directory and prints their frame ranges in the following format:
-#         'name: 1001-2000' if there are no gaps
-#         'name: 1001, 1003-1500, 1600-2000' if there are gaps
-#     the format for an animated sequence is name.####.ext e.g. /job/.../my_render_v001.1001.jpg
 
 import re
 import os
